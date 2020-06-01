dd3621c6cea775040936a001bdcd71c163cc0d1b
pankaj-pant <pankajpant89@gmail.com>
2020-06-01 23:04:36 +0300

    help link

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -2,13 +2,16 @@ import React from 'react'
 import {
   Navbar,
   NavbarBrand,
-
+  NavLink
 } from 'reactstrap'
 
 const AppNavbar = () => {
   return (
     <Navbar color="light" light expand="md">
       <NavbarBrand href="/">Bird Watching App</NavbarBrand>
+      <NavLink href="https://github.com/pankaj-pant/birdy#bird-watching-application" target="_blank" className="ml-auto">
+            Help?
+      </NavLink>
     </Navbar>
   )
 }
